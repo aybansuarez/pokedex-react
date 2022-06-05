95302ca8d35ec782b57fb4c9fe4e767c97a4599f
Ivan Paolo Suarez <jedsuarez@gmail.com>
2022-06-05 22:39:33 +0800

    add background

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,13 @@
 import Header from "./Header";
+import background from "/src/assets/background.png";
 
 function Layout({ children }) {
   return (
-    <div className="flex flex-col">
-      <Header />
-      <main>{children}</main>
+    <div className="bg-fixed" style={{ backgroundImage: `url(${background})` }}>
+      <div className="flex flex-col bg-gradient-to-r from-white">
+        <Header />
+        <main>{children}</main>
+      </div>
     </div>
   );
 }
