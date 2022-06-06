b76cc648fbca56f99430cea0875cdf29a7757de6
Ivan Paolo Suarez <jedsuarez@gmail.com>
2022-06-07 03:26:41 +0800

    add list of pokedexes page

---
+++ src/components/PokedexCard.jsx
@@ -0,0 +1,105 @@
+import { useQuery } from "react-query";
+import { fetchGeneration } from "/src/api/generation";
+import { fetchRegion } from "/src/api/region";
+import { Fragment, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { getGenerationName } from "/src/utils/common";
+
+const getGetStarters = (pokemons, region) => {
+  let start = 0;
+  let starterCount = 3;
+
+  if (region == "unova") {
+    start++;
+    starterCount++;
+  }
+
+  const starters = pokemons.slice(start, starterCount);
+  return starters;
+};
+
+function PokedexCard({ name }) {
+  const [isBgLoaded, setIsBgLoaded] = useState(true);
+
+  const {
+    data: region,
+    isLoading: isRegionLoading,
+    isSuccess: isRegionSuccess,
+  } = useQuery(["region", name], fetchRegion(name), {
+    refetchOnWindowFocus: false,
+  });
+
+  const {
+    data: generation,
+    isLoading: isGenerationLoading,
+    isSuccess: isGenerationSuccess,
+  } = useQuery(
+    ["generation", region?.main_generation?.name],
+    fetchGeneration(region?.main_generation?.name),
+    { enabled: !!region, refetchOnWindowFocus: false }
+  );
+
+  const isSuccess = isRegionSuccess && isGenerationSuccess && isBgLoaded;
+  const isLoading = isRegionLoading || isGenerationLoading || !isSuccess;
+
+  useEffect(() => {
+    if (isRegionSuccess) {
+      const img = new Image();
+      img.onload = () => {
+        setIsBgLoaded(true);
+      };
+      img.src = `/src/assets/regions/${region?.name}.webp`;
+    }
+  }, [isRegionSuccess]);
+
+  console.log(isSuccess);
+  return (
+    <Fragment>
+      {isLoading ? (
+        <div className="relative aspect-video animate-pulse bg-slate-200">
+          <div className="absolute flex w-full flex-col items-center gap-y-1 p-5">
+            <div className="h-6 w-12 rounded-xl bg-slate-300" />
+            <div className="h-12 w-48 rounded-3xl bg-slate-300 xs:h-16 sm:w-60 xl:h-24 xl:w-96" />
+          </div>
+        </div>
+      ) : (
+        <div className="group relative select-none overflow-hidden">
+          <Link to={`/pokedex/${region?.name}`}>
+            <div
+              className="absolute h-full w-full bg-cover bg-center bg-no-repeat brightness-[20%] duration-150 ease-in group-hover:scale-150 group-hover:brightness-50"
+              style={{
+                backgroundImage: `url('/src/assets/regions/${region?.name}.webp')`,
+              }}
+            />
+            <div className="relative flex aspect-video w-full flex-col items-center p-5">
+              <div className="z-10 grid h-full grid-cols-3 content-evenly pt-10 sm:pt-16">
+                {getGetStarters(generation?.pokemon_species, region?.name).map(
+                  (pokemon, index) => {
+                    return (
+                      <img
+                        key={index}
+                        alt={pokemon.name}
+                        className="brightness-0 duration-150 ease-in first:ml-auto last:mr-auto even:mx-auto group-hover:scale-110 group-hover:brightness-100"
+                        src={`https://img.pokemondb.net/sprites/home/normal/${pokemon.name}.png`}
+                      />
+                    );
+                  }
+                )}
+              </div>
+              <div className="absolute font-stencil_one uppercase text-white brightness-75 duration-150 ease-in group-hover:brightness-100">
+                <p className="text-center">
+                  {getGenerationName(generation?.names)}
+                </p>
+                <p className="text-5xl font-black xs:text-6xl md:text-6xl lg:text-7xl xl:text-8xl">
+                  {region?.name}
+                </p>
+              </div>
+            </div>
+          </Link>
+        </div>
+      )}
+    </Fragment>
+  );
+}
+
+export default PokedexCard;
---
+++ src/views/Pokedexes.jsx
@@ -0,0 +1,56 @@
+import Seo from "/src/components/Seo";
+import { Fragment } from "react";
+import { useQuery } from "react-query";
+import PokedexCard from "/src/components/PokedexCard";
+import { Link } from "react-router-dom";
+import { fetchRegions } from "../api/region";
+
+function Pokedexes() {
+  const { data: region } = useQuery(["regions"], fetchRegions(), {
+    refetchOnWindowFocus: false,
+  });
+
+  return (
+    <Fragment>
+      <Seo title="Pokédex" description="Pokémon" lang="en" meta={[]} />
+      <div className="dex-layout mx-auto pt-10">
+        <div className="flex flex-col gap-4">
+          <p className="font-stencil_one text-4xl font-bold uppercase xs:text-5xl md:text-6xl">
+            Pokédex
+          </p>
+        </div>
+        <div className="grid grid-cols-1 gap-2 py-10 sm:grid-cols-2">
+          <div className="group relative select-none overflow-hidden sm:col-span-2">
+            <Link to={`/pokedex/national`}>
+              <div
+                className="absolute h-full w-full bg-cover bg-center bg-no-repeat brightness-[20%] duration-100 ease-in group-hover:scale-150 group-hover:brightness-50"
+                style={{
+                  backgroundImage: `url('/src/assets/regions/galar.webp')`,
+                }}
+              />
+              <div className="relative flex aspect-video w-full flex-col items-center p-5 sm:aspect-auto">
+                <div className="z-10 grid h-full grid-cols-1 content-evenly pt-10 lg:pt-9 xl:pt-16">
+                  <img
+                    className="w-1/3 brightness-0 duration-100 ease-in first:ml-auto last:mr-auto even:mx-auto group-hover:scale-110 group-hover:brightness-100 xs:w-1/2 sm:w-1/3 md:w-1/2 lg:w-2/3"
+                    src={`https://img.pokemondb.net/sprites/home/normal/pikachu.png`}
+                  />
+                </div>
+                <div className="absolute font-stencil_one uppercase text-white brightness-75 duration-100 ease-in group-hover:brightness-100">
+                  <p className="text-center">All Generations</p>
+                  <p className="text-5xl font-black xs:text-6xl md:text-6xl lg:text-7xl xl:text-8xl">
+                    National
+                  </p>
+                </div>
+              </div>
+            </Link>
+          </div>
+          {region?.results.map((region) => {
+            return <PokedexCard key={region.name} name={region.name} />;
+          })}
+        </div>
+      </div>
+    </Fragment>
+  );
+}
+
+export default Pokedexes;
